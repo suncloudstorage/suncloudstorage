14ddce08be5cd36bc4b9dc78ae2163b1aea1c91f
suncloudstorage <suncloudstorage@gmail.com>
2020-08-31 11:26:49 +0300

    Initialize Spring boot application

---
+++ src/main/java/com/suncloudstorage/SunCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package com.suncloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunCloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunCloudStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suncloudstorage/SunCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suncloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunCloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
