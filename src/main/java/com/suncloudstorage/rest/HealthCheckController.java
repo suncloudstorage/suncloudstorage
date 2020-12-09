b2976f8ceb2ae1e690c3e12026418c1e7c1bba81
suncloudstorage <suncloudstorage@gmail.com>
2020-12-09 22:53:00 +0200

    Remove Jenkinsfile

--- src/main/java/com/suncloudstorage/rest/HealthCheckController.java
+++ src/main/java/com/suncloudstorage/rest/HealthCheckController.java
@@ -14,7 +14,7 @@ public class HealthCheckController {
     @GetMapping("/healthcheck")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok().body("SunCloudStorage is running" +
-                ("override".equalsIgnoreCase(appType) ? "with override properties" : ""));
+                ("override".equalsIgnoreCase(appType) ? " with override properties" : ""));
     }
 
 }
